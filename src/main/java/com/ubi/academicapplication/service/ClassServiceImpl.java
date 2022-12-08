75db36e884187ee482faef7a04f75e671fdd3768
sayalipatil12345 <sayaleep96@gmail.com>
2022-12-08 16:53:51 +0530

    Code Refactory

--- src/main/java/com/ubi/academicapplication/service/ClassServiceImpl.java
+++ src/main/java/com/ubi/academicapplication/service/ClassServiceImpl.java
@@ -8,9 +8,9 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
-import com.ubi.academicapplication.dto.responsedto.ClassDto;
 import com.ubi.academicapplication.dto.responsedto.Response;
 import com.ubi.academicapplication.entity.ClassDetail;
 import com.ubi.academicapplication.error.CustomException;
@@ -29,32 +29,34 @@ public class ClassServiceImpl implements ClassService {
 	@Autowired
 	Result res;
 
-	@Override
-	public ClassDetail addClassDetails(ClassDto classDto) {
+	public Response<ClassDetail> addClassDetails(ClassDetail classDetail) {
 
+		Response<ClassDetail> response = new Response<>();
 
-		LOGGER.info("addClassDetails added..");
-		ClassDetail classDetail=new ClassDetail();
-		classDetail.setClassCode(classDto.getClassCode());
-		classDetail.setClassName(classDto.getClassName());
-
-		return classRepository.save(classDetail);
-
+		if (classDetail.getClassCode().isEmpty() || classDetail.getClassCode().length() == 0) {
+			throw new CustomException(HttpStatusCode.NO_CLASSCODE_FOUND.getCode(),
+					HttpStatusCode.NO_CLASSCODE_FOUND, HttpStatusCode.NO_CLASSCODE_FOUND.getMessage(), res);
+		}
+		ClassDetail savedClass = classRepository.save(classDetail);
+		response.setStatusCode(HttpStatusCode.RESOURCE_CREATED_SUCCESSFULLY.getCode());
+		response.setMessage(HttpStatusCode.RESOURCE_CREATED_SUCCESSFULLY.getMessage());
+		response.setResult(new Result<ClassDetail>(savedClass));
+		return response;
 	}
 
-	public Response<List<ClassDetail>> getClassDetails() {
-		Response<List<ClassDetail>> getListofClasses = new Response<List<ClassDetail>>();
-		List<ClassDetail> list = (List<ClassDetail>) this.classRepository.findAll();
-		res.setData(list);
-		Result<List<ClassDetail>> allClassResult = new Result<>();
-		if (list.size() == 0) {
+	public Response<List<ClassDetail>> getClassDetails(Integer PageNumber, Integer PageSize) {
+		res.setData(null);
+		Pageable paging = PageRequest.of(PageNumber, PageSize);
+		Response<List<ClassDetail>> getListofClasses = new Response<>();
+		Page<ClassDetail> list = this.classRepository.findAll(paging);
+		res.setData(list.toList());
+		Result<List<ClassDetail>> result = new Result<>();
+		if (list.getSize() == 0) {
 			throw new CustomException(HttpStatusCode.NO_ENTRY_FOUND.getCode(), HttpStatusCode.NO_ENTRY_FOUND,
 					HttpStatusCode.NO_ENTRY_FOUND.getMessage(), res);
 		}
-
-		allClassResult.setData(list);
 		getListofClasses.setStatusCode(200);
-		getListofClasses.setResult(allClassResult);
+		getListofClasses.setResult(res);
 		return getListofClasses;
 	}
 
@@ -73,30 +75,40 @@ public class ClassServiceImpl implements ClassService {
 		return getClass;
 	}
 
-	@Override
-	public void deleteClassById(Long classidL) {
-
-		LOGGER.info("deleteClassById added..");
-
-		classRepository.deleteById(classidL);
+	public Response<ClassDetail> deleteClassById(Long id) {
+		res.setData(null);
+		Optional<ClassDetail> classDetail = classRepository.findById(id);
+		if (!classDetail.isPresent()) {
+			throw new CustomException(HttpStatusCode.RESOURCE_NOT_FOUND.getCode(), HttpStatusCode.RESOURCE_NOT_FOUND,
+					HttpStatusCode.RESOURCE_NOT_FOUND.getMessage(), res);
+		}
+		classRepository.deleteById(id);
+		Response<ClassDetail> response = new Response<>();
+		response.setMessage(HttpStatusCode.CLASS_DELETED.getMessage());
+		response.setStatusCode(HttpStatusCode.CLASS_DELETED.getCode());
+		response.setResult(new Result<ClassDetail>(classDetail.get()));
+		return response;
 	}
 
-	@Override
-	public ClassDetail updateClassDetails(ClassDetail classDetail) {
-		ClassDetail savedClassDetails = classRepository.save(classDetail);
-		return savedClassDetails;
+	public Response<ClassDetail> updateClassDetails(ClassDetail classDetail) {
+		res.setData(null);
+		Optional<ClassDetail> existingStudentContainer = classRepository.findById(classDetail.getClassId());
+		if (!existingStudentContainer.isPresent()) {
+			throw new CustomException(HttpStatusCode.NO_CLASS_FOUND.getCode(), HttpStatusCode.NO_CLASS_FOUND,
+					HttpStatusCode.NO_CLASS_FOUND.getMessage(), res);
+		}
+		ClassDetail existingStudent = existingStudentContainer.get();
+		existingStudent.setClassCode(classDetail.getClassCode());
+		existingStudent.setClassName(classDetail.getClassName());
 
+		ClassDetail updateStudent = classRepository.save(existingStudent);
+		Response<ClassDetail> response = new Response<>();
+		response.setMessage(HttpStatusCode.CLASS_UPDATED.getMessage());
+		response.setStatusCode(HttpStatusCode.CLASS_UPDATED.getCode());
+		response.setResult(new Result<>(updateStudent));
+		return response;
 	}
-
-	//pagination
-	@Override
-	public Page<ClassDetail> findClassWithPagination(int offset,int pageSize){
-
-		LOGGER.info("findClassWithPagination added..");
-
-		return classRepository.findAll(PageRequest.of(offset, pageSize));
-
-	}
+}
 //	//sorting
 //	@Override
 //	public List<ClassDetail> findClassWithSorting(String field){
@@ -105,4 +117,4 @@ public class ClassServiceImpl implements ClassService {
 
 //		return  classRepository.findAll(Sort.by(Sort.Direction.ASC,field));
 //	}
-}
+
