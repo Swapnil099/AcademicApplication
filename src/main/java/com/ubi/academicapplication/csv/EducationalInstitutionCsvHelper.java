40af68663ad90df3cccee608a1ad865083debe03
Rahul <rahul.aher@neosoftmail.com>
2022-12-17 17:06:40 +0530

    Sort Educational Institution By Name and Code in Ascending Order

--- src/main/java/com/ubi/academicapplication/csv/EducationalInstitutionCsvHelper.java
+++ src/main/java/com/ubi/academicapplication/csv/EducationalInstitutionCsvHelper.java
@@ -17,49 +17,60 @@ import com.ubi.academicapplication.entity.Region;
 import com.ubi.academicapplication.mapper.EducationalInstitutionMapper;
 
 public class EducationalInstitutionCsvHelper {
-	
+
 	@Autowired
 	static EducationalInstitutionMapper educationalInstitutionMapper;
-	
+
 	public static ByteArrayInputStream educationCSV(List<EducationalInstitution> eduInst) {
-	    final CSVFormat format = CSVFormat.DEFAULT.withQuoteMode(QuoteMode.MINIMAL);
-	    int count=0;
-	    try (ByteArrayOutputStream out = new ByteArrayOutputStream();
-	        CSVPrinter csvPrinter = new CSVPrinter(new PrintWriter(out), format);) {
-	      for (EducationalInstitution eduInsts : eduInst) {
-	    	  int m=1;
-	    	 
-	        List<String> data = new ArrayList<>();
-	        String eduId = String.valueOf(eduInsts.getId());
-	        String eduStrength = String.valueOf(eduInsts.getStrength());
-	        String eduAcc = String.valueOf(eduInsts.getVvnAccount());
-	       
-	        data.add(eduId);
-	        data.add(eduInsts.getEducationalInstitutionCode());
-	        data.add(eduInsts.getEducationalInstitutionName());
-	        data.add(eduInsts.getEducationalInstitutionType());
-	        data.add(eduInsts.getState());
-	        data.add(eduInsts.getExemptionFlag());
-	        data.add(eduStrength);
-	        data.add(eduAcc);
-	     
-	        for(Region reg:eduInsts.getRegion()) {	
-	        	String regId = String.valueOf(reg.getId());
-	        	data.add(regId);
-	        	data.add(reg.getCode());
-	        	data.add(reg.getName());
-	        	
-	        }
-	        count +=m;
-	        csvPrinter.printRecord(data);
-	        
-	      }
-	     csvPrinter.printRecord(count);
-	      csvPrinter.flush();
-	      return new ByteArrayInputStream(out.toByteArray());
-	    } catch (IOException e) {
-	      throw new RuntimeException("fail to import data: " + e.getMessage());
-	    }
+		final CSVFormat format = CSVFormat.DEFAULT.withQuoteMode(QuoteMode.MINIMAL);
+		int count = 0;
+		try (ByteArrayOutputStream out = new ByteArrayOutputStream();
+				CSVPrinter csvPrinter = new CSVPrinter(new PrintWriter(out), format);) {
+
+			List<String> header = new ArrayList<>();
+			header.add("Educational Institute Id");
+			header.add("Educational Institution Code");
+			header.add("Educational Institution Name");
+			header.add("Educational Institution Type");
+			header.add("Educational Institution State");
+			header.add("Educational Institution ExemptionFlag");
+			header.add("Educational Institution Strength");
+			header.add("Educational Institution VvnAccount");
+			header.add("Region Id");
+			header.add("Region Code");
+			header.add("Region Name");
+			csvPrinter.printRecord(header);
+			for (EducationalInstitution eduInsts : eduInst) {
+				int m = 1;
+
+				List<String> data = new ArrayList<>();
+
+				data.add(String.valueOf(eduInsts.getId()));
+				data.add(eduInsts.getEducationalInstitutionCode());
+				data.add(eduInsts.getEducationalInstitutionName());
+				data.add(eduInsts.getEducationalInstitutionType());
+				data.add(eduInsts.getState());
+				data.add(eduInsts.getExemptionFlag());
+				data.add(String.valueOf(eduInsts.getStrength()));
+				data.add(String.valueOf(eduInsts.getVvnAccount()));
+
+				for (Region reg : eduInsts.getRegion()) {
+
+					data.add(String.valueOf(reg.getId()));
+					data.add(reg.getCode());
+					data.add(reg.getName());
+
+				}
+				count += m;
+				csvPrinter.printRecord(data);
+
+			}
+			csvPrinter.printRecord("Count: " + count);
+			csvPrinter.flush();
+			return new ByteArrayInputStream(out.toByteArray());
+		} catch (IOException e) {
+			throw new RuntimeException("fail to import data: " + e.getMessage());
+		}
 	}
 
 }
